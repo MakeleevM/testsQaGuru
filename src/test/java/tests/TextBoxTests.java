a934abb5a1759b77285f6307dda0f649674d3a22
Maksat M <m.makeleev27@gmail.com>
2026-03-29 19:20:59 +0300

    added Base Test

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,26 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TextBoxTests {
-
-    @BeforeAll
-    static void beforeAll () {
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "146.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000; // default 4000
-
-
-    }
-
+public class TextBoxTests extends BaseTest{
 
     @Test
     void  successfullFillFormTest() {
