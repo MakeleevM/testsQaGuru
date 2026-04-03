b5e335403d9d57796661ef735706563e21f51fbb
Maksat M <m.makeleev27@gmail.com>
2026-04-03 18:52:39 +0300

    added edits to homework 4

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
