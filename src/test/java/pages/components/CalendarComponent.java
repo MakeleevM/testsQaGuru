71c6b9dcbb2b63d1b3f165b4af1971b251f7e81e
Maksat M <m.makeleev27@gmail.com>
2026-05-15 16:08:18 +0300

    commit

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,15 @@
-package pages.components;
-
-import static com.codeborne.selenide.Selenide.$;
-
 public class CalendarComponent {
 
-    public void setDate(String day, String month, String year) {
-        $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+    private final SelenideElement yearSelect = $(".react-datepicker__year-select");
+    private final SelenideElement monthSelect = $(".react-datepicker__month-select");
 
+    private SelenideElement dayCell(String day) {
+        return $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)");
+    }
+
+    public void setDate(String day, String month, String year) {
+        yearSelect.selectOption(year);
+        monthSelect.selectOption(month);
+        dayCell(day).click();
     }
 }
